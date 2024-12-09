bcc82263fe64f1f8e9b3d1005057acd6aae9d329
unknown <selinaadwoa1@gmail.com>
2024-12-09 12:10:10 -0800

    adding my files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function changeButtonStyle(){
+    button.style.backgroundColor="blue";
+    button.style.color = "yellow";
+}
+
+
+
+
+
+
+
+
+
+
+
+let button = document.querySelector("button");
+button.addEventListener("click", changeButtonStyle);
