2235324fc19d296b203eafd32bc1739475f66f45
unknown <selinaadwoa1@gmail.com>
2024-12-13 14:06:17 -0800

    add all changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 let button = document.querySelector("button"); 
 let close = document.querySelector(".close"); 
-function changeButtonStyle() { button.style.backgroundColor = "blue"; 
-    button.style.color = "yellow"; 
-} function openImage() { let image = document.querySelector(".img"); 
-    image.style.display = "block"; 
-    close.style.display = "block"; 
-    button.style.display = "none"; 
+function changeButtonStyle() {
+ button.style.backgroundColor = "blue"; 
+  button.style.color = "yellow"; 
+} 
+function openImage() { 
+let image = document.querySelector(".img"); 
+  image.style.display = "block"; 
+   close.style.display = "block"; 
+   button.style.display = "none"; 
 } function closeImage() 
 { let image = document.querySelector(".img"); 
-    image.style.display = "none"; 
-    close.style.display = "none"; 
-    button.style.display = "block"; 
+   image.style.display = "none"; 
+   close.style.display = "none"; 
+ button.style.display = "block"; 
 } button.addEventListener("click", () => { changeButtonStyle(); 
 openImage(); }); close.addEventListener("click", closeImage);
+
+
+
