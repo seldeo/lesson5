791519aae687a45b7621f30b080bb00cf9ed8818
unknown <selinaadwoa1@gmail.com>
2024-12-13 13:37:05 -0800

    add all changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-function changeButtonStyle(){
-    button.style.backgroundColor="blue";
-    button.style.color = "yellow";
-}
-
-
-
-
-
-
-
-
-
-
-
-let button = document.querySelector("button");
-button.addEventListener("click", changeButtonStyle);
+let button = document.querySelector("button"); 
+let close = document.querySelector(".close"); 
+function changeButtonStyle() { button.style.backgroundColor = "blue"; 
+    button.style.color = "yellow"; 
+} function openImage() { let image = document.querySelector(".img"); 
+    image.style.display = "block"; 
+    close.style.display = "block"; 
+    button.style.display = "none"; 
+} function closeImage() 
+{ let image = document.querySelector(".img"); 
+    image.style.display = "none"; 
+    close.style.display = "none"; 
+    button.style.display = "block"; 
+} button.addEventListener("click", () => { changeButtonStyle(); 
+openImage(); }); close.addEventListener("click", closeImage);
